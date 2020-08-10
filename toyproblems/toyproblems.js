3309c2dcb8c755a31e34e5d370c8b459d14fcb29
Austin Smith <asmit2222@gmail.com>
2020-08-09 21:28:48 -0400

    finished Make Arrays Equalish

--- toyproblems/toyproblems.js
+++ toyproblems/toyproblems.js
@@ -29,7 +29,7 @@ const ticTacToeSolver = function(board) {
 
 module.exports.ticTacToeSolver = ticTacToeSolver;
 
-// uncomment the tests for each toy problem to test your code. The first one is uncommented for you.
+// uncomment the tests for each toy problem to test your code. The first one (line below) is uncommented for you.
 test.testRunnerTicTacToe();
 
 
@@ -58,6 +58,33 @@ module.exports.findTheNextNumber = findTheNextNumber;
 
 // uncomment the test below
 // test.testRunnerFindTheNextNumber();
+
+
+/* Make Arrays Equal(ish):
+
+- Given 2 arrays of numbers and of different lengths, trade numbers between them to get the totals of the arrays to as close to equal as possible, without changing the length of the arrays.
+- Sort the arrays before returning them within another array.
+
+Example:
+
+Input: [22, 5, 6, 56], [44, 33, 89]
+Output: [[5, 22, 44, 56], [6, 33, 89]]
+
+*/
+
+const makeArraysEqualish = function (arr1, arr2) {
+
+// your code here!
+
+};
+
+module.exports.makeArraysEqualish = makeArraysEqualish;
+
+// uncomment the test below
+// test.testRunnerMakeArraysEqualish();
+
+
+
 
 
 
